20150c95d6d73d6548b75b2dcd90a5260d0d3407
Vinay <rmdort@gmail.com>
2020-07-25 22:53:32 +0800

    Wrap image in memo

--- packages/grid/src/Image.tsx
+++ packages/grid/src/Image.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, memo } from 'react';
 import { Image } from 'react-konva';
 import useImage from './hooks/useImage';
 import { ShapeConfig } from 'konva/types/Shape';
@@ -8,7 +8,7 @@ export interface ImageProps extends ShapeConfig {
   spacing?: number
 }
 
-const ImageComponent: React.FC<ImageProps> = (props) => {
+const ImageComponent: React.FC<ImageProps> = memo((props) => {
   let { url, width = 0, height = 0, x = 0, y = 0, spacing = 1, ...rest } = props
   const { image, width: imageWidth, height: imageHeight, status } = useImage({ url });  
   const aspectRatio = useMemo(() => {
@@ -25,6 +25,6 @@ const ImageComponent: React.FC<ImageProps> = (props) => {
   return (
     <Image {...rest} x={x} y={y} height={height} width={width} image={image} />
   );
-}
+})
 
 export default ImageComponent
